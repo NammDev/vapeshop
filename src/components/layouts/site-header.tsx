5af67c9e419d0561ecbdd0f381a1c95b02706519
Nam <namdeveloper.ca@gmail.com>
2024-04-15 00:01:06 +0700

    mainnav mobile

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -2,9 +2,9 @@ import type { User } from '@clerk/nextjs/server'
 
 import { siteConfig } from '@/config/site'
 import { MainNav } from './main-nav'
+import { MobileNav } from './mobile-nav'
 // import { CartSheet } from '@/components/checkout/cart-sheet'
 // import { AuthDropdown } from '@/components/layouts/auth-dropdown'
-// import { MobileNav } from '@/components/layouts/mobile-nav'
 // import { ProductsCommandMenu } from '@/components/products-command-menu'
 
 interface SiteHeaderProps {
@@ -16,7 +16,7 @@ export function SiteHeader({ user }: SiteHeaderProps) {
     <header className='sticky top-0 z-50 w-full border-b bg-background'>
       <div className='container flex h-16 items-center'>
         <MainNav items={siteConfig.mainNav} />
-        {/* <MobileNav items={siteConfig.mainNav} /> */}
+        <MobileNav items={siteConfig.mainNav} />
         <div className='flex flex-1 items-center justify-end space-x-4'>
           <nav className='flex items-center space-x-2'>
             Header
