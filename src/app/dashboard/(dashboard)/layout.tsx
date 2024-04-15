5edf169908157d3ae1d6d238c0e8e6364a842c62
Nam <namdeveloper.ca@gmail.com>
2024-04-15 15:44:03 +0700

    layout for dashboard

---
+++ src/app/dashboard/(dashboard)/layout.tsx
@@ -0,0 +1,54 @@
+import { redirect } from 'next/navigation'
+
+// import { getStoresByUserId } from '@/lib/actions/store'
+// import { getCacheduser, getProgress } from '@/lib/actions/user'
+import { SiteFooter } from '@/components/layouts/site-footer'
+import { SidebarProvider } from '../_components/sidebar-provider'
+
+// import { DashboardHeader } from './_components/dashboard-header'
+// import { DashboardSidebar } from './_components/dashboard-sidebar'
+// import { DashboardSidebarSheet } from './_components/dashboard-sidebar-sheet'
+// import { StoreSwitcher } from './_components/store-switcher'
+
+export default async function DashboardLayout({ children }: React.PropsWithChildren) {
+  //   const user = await getCacheduser()
+
+  //   if (!user) {
+  //     redirect('/signin')
+  //   }
+
+  //   const storesPromise = getStoresByUserId({ userId: user.id })
+  //   const progressPromise = getProgress({ userId: user.id })
+
+  return (
+    <SidebarProvider>
+      <div className='flex min-h-screen flex-col'>
+        {/* <DashboardHeader user={user}>
+          <DashboardSidebarSheet className='lg:hidden'>
+            <DashboardSidebar className='pl-4'>
+              <StoreSwitcher
+                userId={user.id}
+                storesPromise={storesPromise}
+                progressPromise={progressPromise}
+              />
+            </DashboardSidebar>
+          </DashboardSidebarSheet>
+        </DashboardHeader> */}
+        <div className='container flex-1 items-start lg:grid lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10'>
+          {/* <DashboardSidebar
+            // the top-16 class is used for the dashboard-header of h-16, added extra 0.1rem to fix the sticky layout shift issue
+            className="top-[calc(theme('spacing.16')_+_0.1rem)] z-30 hidden border-r lg:sticky lg:block"
+          >
+            <StoreSwitcher
+              userId={user.id}
+              storesPromise={storesPromise}
+              progressPromise={progressPromise}
+            />
+          </DashboardSidebar> */}
+          <main className='flex w-full flex-col overflow-hidden'>{children}</main>
+        </div>
+        <SiteFooter />
+      </div>
+    </SidebarProvider>
+  )
+}
---
+++ src/app/dashboard/(dashboard)/stores/page.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import type { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+// import { env } from '@/env.js'
+
+// import { getStoresByUserId } from '@/lib/actions/store'
+// import { getCacheduser, getProgress } from '@/lib/actions/user'
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/app-ui/page-header'
+import { Shell } from '@/components/app-ui/shell'
+import { StoreCardSkeleton } from '@/components/skeletons/store-card-skeleton'
+
+// import { AddStoreDialog } from './_components/add-store-dialog'
+// import { Stores } from './_components/stores'
+
+export const metadata: Metadata = {
+  //   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: 'Stores',
+  description: 'Manage your stores',
+}
+
+export default async function StoresPage() {
+  //   const user = await getCacheduser()
+
+  //   if (!user) {
+  //     redirect('/signin')
+  //   }
+
+  //   const storesPromise = getStoresByUserId({ userId: user.id })
+  //   const progressPromise = getProgress({ userId: user.id })
+
+  return (
+    <Shell variant='sidebar'>
+      <PageHeader>
+        <div className='flex space-x-4'>
+          <PageHeaderHeading size='sm' className='flex-1'>
+            Stores
+          </PageHeaderHeading>
+          {/* <AddStoreDialog userId={user.id} progressPromise={progressPromise} /> */}
+        </div>
+        <PageHeaderDescription size='sm'>Manage your stores</PageHeaderDescription>
+      </PageHeader>
+      <section className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+        <React.Suspense
+          fallback={Array.from({ length: 3 }).map((_, i) => (
+            <StoreCardSkeleton key={i} />
+          ))}
+        >
+          Stores
+          {/* <Stores storesPromise={storesPromise} /> */}
+        </React.Suspense>
+      </section>
+    </Shell>
+  )
+}
