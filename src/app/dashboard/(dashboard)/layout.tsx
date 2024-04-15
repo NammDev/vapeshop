84157b444679842561d27252844c5da0d39adab5
Nam <namdeveloper.ca@gmail.com>
2024-04-15 16:03:04 +0700

    dashboard sidebar

--- src/app/dashboard/(dashboard)/layout.tsx
+++ src/app/dashboard/(dashboard)/layout.tsx
@@ -1,14 +1,13 @@
 import { redirect } from 'next/navigation'
 
 // import { getStoresByUserId } from '@/lib/actions/store'
-// import { getCacheduser, getProgress } from '@/lib/actions/user'
+// import {  getProgress } from '@/lib/actions/user'
 import { SiteFooter } from '@/components/layouts/site-footer'
 import { SidebarProvider } from '../_components/sidebar-provider'
-import { DashboardHeader } from '../_components/dashboard.header'
+import { DashboardHeader } from '../_components/dashboard-header'
 import { getCacheduser } from '@/lib/actions/user'
+import { DashboardSidebar } from '../_components/dashboard-sidebar'
 
-// import { DashboardHeader } from './_components/dashboard-header'
-// import { DashboardSidebar } from './_components/dashboard-sidebar'
 // import { DashboardSidebarSheet } from './_components/dashboard-sidebar-sheet'
 // import { StoreSwitcher } from './_components/store-switcher'
 
@@ -38,16 +37,17 @@ export default async function DashboardLayout({ children }: React.PropsWithChild
           </DashboardSidebarSheet> */}
         </DashboardHeader>
         <div className='container flex-1 items-start lg:grid lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10'>
-          {/* <DashboardSidebar
+          <DashboardSidebar
             // the top-16 class is used for the dashboard-header of h-16, added extra 0.1rem to fix the sticky layout shift issue
             className="top-[calc(theme('spacing.16')_+_0.1rem)] z-30 hidden border-r lg:sticky lg:block"
           >
-            <StoreSwitcher
+            {/* <StoreSwitcher
               userId={user.id}
               storesPromise={storesPromise}
               progressPromise={progressPromise}
-            />
-          </DashboardSidebar> */}
+            /> */}
+            Store Switcher
+          </DashboardSidebar>
           <main className='flex w-full flex-col overflow-hidden'>{children}</main>
         </div>
         <SiteFooter />
---
+++ src/app/dashboard/_components/dashboard-sidebar.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { dashboardConfig } from '@/config/dashboard'
+import { SidebarNav } from '@/components/layouts/sidebar-nav'
+
+interface DashboardSidebarProps extends React.HTMLAttributes<HTMLElement> {
+  children: React.ReactNode
+}
+
+export function DashboardSidebar({ children, className, ...props }: DashboardSidebarProps) {
+  return (
+    <aside className={cn('w-full', className)} {...props}>
+      <div className='pr-6 pt-4 lg:pt-6'>{children}</div>
+      <ScrollArea className='h-[calc(100vh-8rem)] py-2.5 pr-6'>
+        <SidebarNav items={dashboardConfig.sidebarNav} className='p-1 pt-4' />
+      </ScrollArea>
+    </aside>
+  )
+}
