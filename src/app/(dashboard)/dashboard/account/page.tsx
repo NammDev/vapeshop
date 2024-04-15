db5273805abc9d9c658bae96637ba146bcf10dce
Nam <namdeveloper.ca@gmail.com>
2024-04-15 16:09:21 +0700

    dasboard/account

---
+++ src/app/(dashboard)/dashboard/account/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+// import { env } from '@/env.js'
+
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/app-ui/page-header'
+import { Shell } from '@/components/app-ui/shell'
+
+import { UserProfile } from './_components/user-profile'
+
+export const metadata: Metadata = {
+  //   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: 'Account',
+  description: 'Manage your account settings',
+}
+
+export default function AccountPage() {
+  return (
+    <Shell variant='sidebar'>
+      <PageHeader>
+        <PageHeaderHeading size='sm'>Account</PageHeaderHeading>
+        <PageHeaderDescription size='sm'>Manage your account settings</PageHeaderDescription>
+      </PageHeader>
+      <section className='w-full overflow-hidden'>
+        <UserProfile />
+      </section>
+    </Shell>
+  )
+}
