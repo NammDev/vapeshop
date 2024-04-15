4d904a764892413cd52a107ae2d08a3faf8282c2
Nam <namdeveloper.ca@gmail.com>
2024-04-15 15:06:28 +0700

    reset password confirm & form

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -28,16 +28,16 @@ export const checkEmailSchema = z.object({
   email: authSchema.shape.email,
 })
 
-// export const resetPasswordSchema = z
-//   .object({
-//     password: authSchema.shape.password,
-//     confirmPassword: authSchema.shape.password,
-//     code: verifyEmailSchema.shape.code,
-//   })
-//   .refine((data) => data.password === data.confirmPassword, {
-//     message: 'Passwords do not match',
-//     path: ['confirmPassword'],
-//   })
+export const resetPasswordSchema = z
+  .object({
+    password: authSchema.shape.password,
+    confirmPassword: authSchema.shape.password,
+    code: verifyEmailSchema.shape.code,
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  })
 
 // export const userPrivateMetadataSchema = z.object({
 //   role: z.enum(['user', 'admin', 'super_admin']),
