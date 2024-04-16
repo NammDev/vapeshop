192a0697f6867e87c3b16445427db5959689c090
Nam <namdeveloper.ca@gmail.com>
2024-04-16 13:11:19 +0700

    products page

---
+++ src/app/(lobby)/products/page.tsx
@@ -0,0 +1,35 @@
+import { type Metadata } from 'next'
+// import { env } from '@/env.js'
+import type { SearchParams } from '@/types'
+
+import { getProducts } from '@/lib/actions/product'
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/app-ui/page-header'
+import { Shell } from '@/components/app-ui/shell'
+
+export const metadata: Metadata = {
+  //   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: 'Products',
+  description: 'Buy products from our stores',
+}
+
+interface ProductsPageProps {
+  searchParams: SearchParams
+}
+
+export default async function ProductsPage({ searchParams }: ProductsPageProps) {
+  const productsTransaction = await getProducts(searchParams)
+
+  return (
+    <Shell>
+      <PageHeader>
+        <PageHeaderHeading size='sm'>Products</PageHeaderHeading>
+        <PageHeaderDescription size='sm'>Buy products from our stores</PageHeaderDescription>
+      </PageHeader>
+      {/* <AlertCard /> */}
+    </Shell>
+  )
+}
