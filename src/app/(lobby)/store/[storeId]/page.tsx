1bef9b40d1f9fae384cb55a3ec64fffc71998f9d
Nam <namdeveloper.ca@gmail.com>
2024-04-16 16:15:34 +0700

    StoreId Page

--- src/app/(lobby)/store/[storeId]/page.tsx
+++ src/app/(lobby)/store/[storeId]/page.tsx
@@ -2,33 +2,27 @@ import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
 import { products, stores } from '@/db/schema'
-// import { env } from '@/env.js'
-import type { SearchParams } from '@/types'
+// import { env } from '@/env.mjs'
 import { eq } from 'drizzle-orm'
 
-import { getProducts } from '@/lib/actions/product'
-import { getStores } from '@/lib/actions/store'
 import { Separator } from '@/components/ui/separator'
-import { Products } from '@/components/app-ui/products'
 import { Shell } from '@/components/app-ui/shell'
+import { Breadcrumbs } from '@/components/app-ui/breadcrumbs'
 
 interface StorePageProps {
   params: {
     storeId: string
   }
-  searchParams: SearchParams
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
 }
 
 async function getStoreFromParams(params: StorePageProps['params']) {
-  const storeId = decodeURIComponent(params.storeId)
-
-  const store = await db.query.stores.findFirst({
+  const storeId = params.storeId
+  return await db.query.stores.findFirst({
     where: eq(stores.id, storeId),
   })
-
-  if (!store) return null
-
-  return store
 }
 
 export async function generateMetadata({ params }: StorePageProps): Promise<Metadata> {
@@ -58,16 +52,40 @@ export default async function StorePage({ params, searchParams }: StorePageProps
   const limit = typeof per_page === 'string' ? parseInt(per_page) : 8
   const offset = typeof page === 'string' ? (parseInt(page) - 1) * limit : 0
 
-  const productsTransaction = await getProducts(searchParams)
+  // const productsTransaction = await getProducts({
+  //   limit: limit,
+  //   offset: offset,
+  //   store_ids: String(store.id),
+  // })
+
+  // const pageCount = Math.ceil(productsTransaction.count / limit)
 
   // Stores transaction
   const storesLimit = 25
   const storesOffset = typeof store_page === 'string' ? (parseInt(store_page) - 1) * storesLimit : 0
 
-  const storesTransaction = await getStores(searchParams)
+  // const storesTransaction = await getStores({
+  //   limit: storesLimit,
+  //   offset: storesOffset,
+  //   sort: 'name.asc',
+  // })
+
+  // const storePageCount = Math.ceil(storesTransaction.count / storesLimit)
 
   return (
     <Shell>
+      <Breadcrumbs
+        segments={[
+          {
+            title: 'Stores',
+            href: '/stores',
+          },
+          {
+            title: store.name,
+            href: `/store/${store.id}`,
+          },
+        ]}
+      />
       <div className='flex flex-col gap-8 md:flex-row md:gap-16'>
         <div className='flex w-full flex-col gap-4'>
           <div className='space-y-2'>
@@ -76,11 +94,11 @@ export default async function StorePage({ params, searchParams }: StorePageProps
           </div>
           <Separator className='my-1.5' />
           {/* <Products
-            products={productsTransaction.data}
-            pageCount={productsTransaction.pageCount}
-            categories={Object.values(products.category.enumValues)}
-            stores={storesTransaction.data}
-            storePageCount={storesTransaction.pageCount}
+            products={productsTransaction.items}
+            pageCount={pageCount}
+            // categories={Object.values(products.category.enumValues)}
+            stores={storesTransaction.items}
+            storePageCount={storePageCount}
           /> */}
         </div>
       </div>
