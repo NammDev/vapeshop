3c0e79cfb980bf5047dbf7198a0d39db391c31dc
Nam <namdeveloper.ca@gmail.com>
2024-04-16 14:31:23 +0700

    intercept modal

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,16 +1,23 @@
 import { currentUser } from '@clerk/nextjs'
-import { PropsWithChildren } from 'react'
 
 import { SiteFooter } from '@/components/layouts/site-footer'
 import { SiteHeader } from '@/components/layouts/site-header'
 
-export default async function LobyLayout({ children }: PropsWithChildren) {
+interface MainLayoutProps
+  extends React.PropsWithChildren<{
+    modal: React.ReactNode
+  }> {}
+
+export default async function LobyLayout({ children, modal }: MainLayoutProps) {
   const user = await currentUser()
 
   return (
     <div className='relative flex min-h-screen flex-col'>
       <SiteHeader user={user} />
-      <main className='flex-1'>{children}</main>
+      <main className='flex-1'>
+        {children}
+        {modal}
+      </main>
       <SiteFooter />
     </div>
   )
---
+++ src/app/(lobby)/product/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default async function ProductPage() {
+  return <h1>Hello </h1>
+}
